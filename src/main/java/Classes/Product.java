b1a52bf631b81f9dfc782d89a533e5d51c66047b
SahiDemon <gsahindu@gmail.com>
2023-12-29 00:06:57 +0530

    main

---
+++ src/main/java/Classes/Product.java
@@ -0,0 +1,98 @@
+package Classes;
+
+
+
+public class Product {
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private String image;
+    private int quantity;
+    private String category;
+
+    public Product() {
+        // Default constructor required for JSP EL
+    }
+
+    public Product(int id, String name, String description, double price, String image, int quantity, String category) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.image = image;
+        this.quantity = quantity;
+        this.category = category;
+    }
+
+    // Getters and setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", image='" + image + '\'' +
+                ", quantity=" + quantity +
+                ", category='" + category + '\'' +
+                '}';
+    }
+}
